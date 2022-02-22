4c5c1ce9108bf3b6caafb2d7d46ce1aed4de76ed
Paulo Oliveira <paulocivictor@gmail.com>
2022-02-22 00:59:11 -0300

    renderizando apostila nodejs como partials handlebars

--- routes/docs/index.js
+++ routes/docs/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
 const path = require('path')
 
 const viewsPath = path.join(__dirname,'../../views')
+
+const app = express();
 
 router.get('/nodejs', (req, res) => {
     const pagina={
@@ -11,7 +14,7 @@ router.get('/nodejs', (req, res) => {
         title: 'Node.js Documentation',
     }
 
-    const secoes = [
+    const arr_secoes = [
         'secao1-introducao',
         'secao2-fundamentos',
         'secao3-core-modules',
@@ -23,11 +26,39 @@ router.get('/nodejs', (req, res) => {
         'secaofinal-aprofundarEm',
     ]
 
+    const secoes = []
+
+    arr_secoes.forEach(secao => {
+        //const partial= require(`${viewsPath}/partials/docs/nodejs/${secao}.handlebars`)
+        const obj_secao = {
+            id: secao,
+        }
+        if(secao !== 'secaofinal-aprofundarEm'){
+            obj_secao.visu = true;
+        }else{
+            obj_secao.visu = false;
+        }
+        secoes.push(obj_secao)
+    })
+
     //tentativa de renderizar usando react
     //res.render(`${__dirname}/render/src`) 
     
     // renderização padrão com handlebars
-    res.render(`${__dirname}/nodejs/nodejs2.handlebars`,{pagina, secoes, layout: 'layout-docs'})
+    res.render(
+        `${__dirname}/nodejs/nodejs2.handlebars`,
+        {
+            pagina, 
+            secoes, 
+            layout: 'layout-docs', 
+            helpers:{
+                partialDocs: function(secao){
+                    //return fs.readFileSync(viewsPath + '\\partials\\docs\\nodejs\\' + secao + ".handlebars", 'utf-8')
+                    return ('docs/nodejs/' + secao)
+                } 
+            }
+        }
+    )
 })
 
 router.get('/',(req,res) => {
